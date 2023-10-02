b19ba9ecf4867bbc80f42320f9b69263a7616ec1
Nikita Agarwal <144547323+Nikita7963@users.noreply.github.com>
2023-10-02 19:06:37 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail ="nikita.agarwal7963@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;//if the variable has no value, it gives undefined as an output
+
+//accountId = 1623589; // it can not be updated as it belong to const data type
+accountEmail ="nikita@gmail.com";
+accountPassword ="6789";
+accountCity = "Bangalore";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //treats all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +3); //code readability should be high
+
+let name ="Nikita";
+let age = 25;
+let isLoggedIn = false;
+let state;
+
+//1.Primitive data types:-
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false;
+//null=> standalone value
+//undefined => when vaueis not assigned to the variable
+//symbol => unique
+
+//object data types:-
+
+console.log(typeof "Nikita")
+console.log(typeof age)
+console.log(typeof null);      //type is object
+console.log(typeof undefined); // type is undefined     
